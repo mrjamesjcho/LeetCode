180dd8696664d50d42d179eff50886855c920a9c
James Cho <jimothycodes@gmail.com>
2025-05-16 12:50:16 -0700

    File updated at 5/16/2025, 12:50:16 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.ts
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.ts
@@ -1,68 +1,12 @@
-// Last updated: 5/16/2025, 12:48:15 PM
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
-function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
-    const countNodes = (node: ListNode | null, k: number): boolean => {
-        let count = 0;
-        while (node !== null && count < k) {
-            node = node.next;
-            count++;
+// Last updated: 5/16/2025, 12:50:16 PM
+function twoSum(nums: number[], target: number): number[] {
+    const diffObj = {};
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const diff = target - num;
+        if (diffObj[diff] !== undefined) {
+            return [diffObj[diff], i];
         }
-        return count === k;
-    };
-
-    const reverse = (head: ListNode | null, k: number): ListNode | null => {
-        let prev: ListNode | null = null;
-        let curr: ListNode | null = head;
-        while (k > 0 && curr !== null) {
-            let next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-            k--;
-        }
-        return prev;
-    };
-
-    let dummy = new ListNode(0, head);
-    let groupPrev: ListNode = dummy;
-
-    while (countNodes(groupPrev.next, k)) {
-        let kth: ListNode | null = groupPrev;
-        for (let i = 0; i < k; i++) {
-            kth = kth!.next;
-        }
-
-        let groupNext = kth!.next;
-        // Reverse k nodes
-        let prev = groupPrev.next;
-        let curr = groupPrev.next;
-        let next: ListNode | null = null;
-
-        // Actual in-place reversal
-        let tail = curr;
-        for (let i = 0; i < k; i++) {
-            next = curr!.next;
-            curr!.next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        let temp = groupPrev.next;
-        groupPrev.next!.next = curr;
-        groupPrev.next = prev;
-        groupPrev = temp!;
+        diffObj[num] = i;
     }
-
-    return dummy.next;
-}
+};
