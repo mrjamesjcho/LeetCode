3e4eced9d10889ae4ee745b6b53cb1f7fa05cf47
James Cho <jimothycodes@gmail.com>
2025-05-16 12:53:55 -0700

    File updated at 5/16/2025, 12:53:55 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.ts
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.ts
@@ -1,33 +1,17 @@
-// Last updated: 5/16/2025, 12:50:43 PM
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    const val1 = l1 ? l1.val : 0;
-    const val2 = l2 ? l2.val : 0;
-    let sum =  val1 + val2;
-    let extraListNode = null;
-    if (sum >= 10) {
-        sum -= 10;
-        if (l1 && l1.next) {
-            l1.next.val++;
-        } else if (l2 && l2.next) {
-            l2.next.val++;
+// Last updated: 5/16/2025, 12:53:55 PM
+function lengthOfLongestSubstring(s: string): number {
+    let longestSubstring = "";
+    let currentSubstring = "";
+    [...s].forEach((char, index) => {
+        const existingCharIndex = currentSubstring.indexOf(char);
+        if (existingCharIndex > -1) {
+            currentSubstring = currentSubstring.substring(existingCharIndex + 1) + char;
         } else {
-            extraListNode = new ListNode(1, null);
+            currentSubstring += char;
         }
-    }
-    if (l1 && l1.next || l2 && l2.next) {
-        return new ListNode(sum, addTwoNumbers(l1 ? l1.next : null, l2 ? l2.next : null))
-    }
-    return new ListNode(sum, extraListNode);
+        if (currentSubstring.length > longestSubstring.length) {
+            longestSubstring = currentSubstring;
+        }
+    });
+    return longestSubstring.length;
 };
