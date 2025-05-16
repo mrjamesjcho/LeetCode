58ec12eef6e301208f26eed5b72c00f80ceea782
James Cho <jimothycodes@gmail.com>
2025-05-16 12:50:43 -0700

    File updated at 5/16/2025, 12:50:43 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.ts
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.ts
@@ -1,12 +1,33 @@
-// Last updated: 5/16/2025, 12:50:16 PM
-function twoSum(nums: number[], target: number): number[] {
-    const diffObj = {};
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        const diff = target - num;
-        if (diffObj[diff] !== undefined) {
-            return [diffObj[diff], i];
+// Last updated: 5/16/2025, 12:50:43 PM
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+    let sum =  val1 + val2;
+    let extraListNode = null;
+    if (sum >= 10) {
+        sum -= 10;
+        if (l1 && l1.next) {
+            l1.next.val++;
+        } else if (l2 && l2.next) {
+            l2.next.val++;
+        } else {
+            extraListNode = new ListNode(1, null);
         }
-        diffObj[num] = i;
     }
+    if (l1 && l1.next || l2 && l2.next) {
+        return new ListNode(sum, addTwoNumbers(l1 ? l1.next : null, l2 ? l2.next : null))
+    }
+    return new ListNode(sum, extraListNode);
 };
